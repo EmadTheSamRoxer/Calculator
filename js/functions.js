82162a06319f94c44875230bfaca2643138ca344
Emad <ejyption@gmail.com>
2019-04-09 20:28:55 +1200

    Initial HTML file, JavaScript file and css file
    This initial commit contains the index.html file where the calculator UI is. A form consisting of 3 inputs with their respective labels, and a button that triggers the form to submit.
    The JavaScript file contains:
    a shell/empty function called 'calculate' always returning 0
    a function to handle form submission, reading in the input values and calling the calculate function with those values.

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+function formSubmit(e){
+	e.preventDefault();
+	
+	let item_amount = document.getElementById("item-amount").value;
+	let item_price = document.getElementById("item-price").value;
+	let state_code = document.getElementById("state-code").value;
+	
+	let total = calculate(item_amount, item_price, state_code);
+	document.getElementById("result").innerHTML = `Total : $${total}`;
+}
+
+
+
+function calculate(amount, price, state_code){
+	return 0;
+}
